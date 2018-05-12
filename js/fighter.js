45974637f2901db7db61c9a6aa7b45832eb18ff4
vitalii0o <frankovitalii@gmail.com>
2018-05-12 09:48:04 -0400

    Added Fighter class

---
+++ js/fighter.js
@@ -0,0 +1,17 @@
+class Fighter {
+    constructor (name, power, health) {
+        this.name = name;
+        this.power = power;
+        this.health = health;
+    }
+
+    setDamage(damage) {
+        this.health -= damage;
+        console.log('health: ' + this.health);
+    }
+
+    hit(enemy, point) {
+        let damage = point * enemy.power;
+        enemy.setDamage(damage);
+    }
+}
