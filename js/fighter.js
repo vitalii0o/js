0538ef7590fbc905727e315d634e3f808d235845
vitalii0o <frankovitalii@gmail.com>
2018-05-13 11:29:36 -0400

    Added isAlive function

--- js/fighter.js
+++ js/fighter.js
@@ -7,11 +7,22 @@ class Fighter {
 
     setDamage(damage) {
         this.health -= damage;
-        console.log(`${this.name} health: ${this.health}`);
+        console.log(`${this.name}'s health: ${this.health}`);
     }
 
     hit(enemy, point = 10) {
         let damage = point * this.power;
+        console.log(`${this.name} hit ${enemy.name} with  ${damage} damage`);
         enemy.setDamage(damage);
+    }
+
+    isAlive() {
+        let isAlive = this.health >= 0;
+        if (isAlive) {
+            console.log(`${this.name}: 'I am still alive.'`);
+        } else {
+            console.log(`${this.name} is dead.`);
+        }
+        return isAlive;
     }
 }
